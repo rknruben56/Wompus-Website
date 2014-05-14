f03ee14fabe757b66cd47f83bb944d1c8dcb89c3
rknruben56 <rodrigrd.rosehulman@gmail.com>
2014-05-13 22:18:09 -0500

    cleaning up code

--- Wompus_Website/Controllers/HomeController.cs
+++ Wompus_Website/Controllers/HomeController.cs
@@ -28,8 +28,6 @@ namespace Wompus_Website.Controllers
         //Partial View for the Sound in the Home Page
         public ActionResult _Music()
         {
-            ViewBag.Message = "Music Will go here!";
-
             return PartialView();
         }
 
@@ -41,7 +39,6 @@ namespace Wompus_Website.Controllers
             var show = from s in db.Shows where EntityFunctions.DiffDays(s.ShowDate, DateTime.Now) < 0 orderby s.ShowDate select s;
 
             //Take only the one closest to the current date
-            //show = show.OrderByDescending(u => u.ShowDate);
             show = (IOrderedQueryable<Wompus_Website.Models.Show>) show.Take(1);
 
             return PartialView(show);
@@ -49,22 +46,6 @@ namespace Wompus_Website.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
